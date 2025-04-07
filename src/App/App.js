1a8738c5a3a473b7cde81169274e9f751c0aad5a
Mark Kendall <mkendall.tutor@gmail.com>
2025-04-06 23:13:26 -0500

    Cleanup: slight code reorganizing, comment pruning

--- src/App/App.js
+++ src/App/App.js
@@ -6,35 +6,15 @@ import { useState, useEffect } from 'react';
 import moviePosters from '../data/movie_posters';
 // import movieDetails from '../data/movie_details';
 import MoviesContainer from '../MoviesContainer/MoviesContainer';
-import MoviePoster from '../MoviePoster/MoviePoster'
+// import MoviePoster from '../MoviePoster/MoviePoster'
 
 function App() {
   const [movies, setMovies] = useState(moviePosters);
-  const allMoviePosters = moviePosters.map(movie => {
-    return (
-      <MoviePoster 
-      id={movie.id} 
-      key={movie.id}
-      title={movie.title} 
-      posterPath={movie.poster_path} 
-      voteCount={movie.vote_count}/>
-    );
-  })
 
-  console.log("movies: ", movies)
-
-  // return (
-  //   <main className='App'>
-  //     <header>
-  //       <h1>rancid tomatillos</h1>
-  //     </header>
-  //       {allMoviePosters}
-  //   </main>
-  // );
   return (
     <main className='App'>
       <header>
-        <h1>rancid tomatillos</h1>
+        <h1>Rancid Tomatillos</h1>
       </header>
         <MoviesContainer movies={movies} />
     </main>
--- src/MoviesContainer/MoviesContainer.js
+++ src/MoviesContainer/MoviesContainer.js
@@ -2,27 +2,16 @@ import './MoviesContainer.css';
 import MoviePoster from '../MoviePoster/MoviePoster';
 
 function Movies({ movies }) {
-
-  // debugger
-
-  // const movieTitles = movies.map(movie => {
-  //   // debugger;
-
-
-  // })
   const moviePoster = movies.map(movie => {
-    // debugger;
-
     return (
       <MoviePoster 
-      id={movie.id} 
-      key={movie.id}
-      title={movie.title} 
-      posterPath={movie.poster_path} 
-      voteCount={movie.vote_count}/>
-      // <MoviePoster movie={movie}/>
+        id={movie.id} 
+        key={movie.id}
+        title={movie.title} 
+        posterPath={movie.poster_path} 
+        voteCount={movie.vote_count}
+      />
     );
-    
   })
 
   return (
@@ -30,7 +19,6 @@ function Movies({ movies }) {
       {moviePoster}
     </div>
   )
-
 }
   
 export default Movies;
