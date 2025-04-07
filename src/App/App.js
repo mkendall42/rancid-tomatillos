223e49aace5e66fda56d72e6a7fee5dd13fcd613
little-Patrick <Little.patrick@outlook.com>
2025-04-07 16:05:07 -0600

    Add: state for swapping bewteen the main and movie details page and onClick methods for doing so.

--- src/App/App.js
+++ src/App/App.js
@@ -1,31 +1,39 @@
 import './App.css';
-import searchIcon from '../icons/search.png';
-// testing 123...
-// Example imports (for later):
+// import searchIcon from '../icons/search.png';
+import homeButton from '../icons/home.png'
 import { useState, useEffect } from 'react';
 import moviePosters from '../data/movie_posters';
-// import movieDetails from '../data/movie_details';
+import movieDetailsData from '../data/movie_details';
+import MovieDetails from '../MovieDetails/MovieDetails.js'
 import MoviesContainer from '../MoviesContainer/MoviesContainer';
-import MoviePoster from '../MoviePoster/MoviePoster'
 
 function App() {
-  const [movies, setMovies] = useState([moviePosters]);
-  const allMoviePosters = moviePosters.map(movie => {
-    return (
-      <MoviePoster 
-      id={movie.id} 
-      key={movie.id}
-      title={movie.title} 
-      posterPath={movie.poster_path} 
-      voteCount={movie.vote_count}/>
-    );
-  })
+  const [movies, setMovies] = useState(moviePosters);
+  const [selectedMovie, setSelectedMovie] = useState(false);
+  
+  function movieClick() {
+    setSelectedMovie(movieDetailsData);
+  }
+  
+  function homeClick() {
+    setSelectedMovie(false);
+  }
+  
   return (
     <main className='App'>
       <header>
         <h1>rancid tomatillos</h1>
+        {selectedMovie && (<button onClick={homeClick}><img src={homeButton} alt="Home" /></button>
+        )}
       </header>
-        {allMoviePosters}
+      {!selectedMovie ? ( <MoviesContainer movies={movies} onMovieClick={movieClick} />) : (
+        <MovieDetails
+          posterPath={selectedMovie.backdrop_path}
+          genreIds={selectedMovie.genres}
+          overview={selectedMovie.overview}
+          title={selectedMovie.title}
+        />
+      )}
     </main>
   );
 }
