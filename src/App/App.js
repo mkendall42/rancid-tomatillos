cfba8ed99b82fe6c3edb76ff365e78c1fa65be8f
little-Patrick <Little.patrick@outlook.com>
2025-04-08 16:04:40 -0600

    Add: routing for getting all movies and getting details for a single movie

--- src/App/App.js
+++ src/App/App.js
@@ -2,23 +2,50 @@ import './App.css';
 // import searchIcon from '../icons/search.png';
 import homeButton from '../icons/home.png'
 import { useState, useEffect } from 'react';
-import moviePosters from '../data/movie_posters';
-import movieDetailsData from '../data/movie_details';
+// import moviePosters from '../data/movie_posters';
+// import movieDetailsData from '../data/movie_details';
 import MovieDetails from '../MovieDetails/MovieDetails.js'
 import MoviesContainer from '../MoviesContainer/MoviesContainer';
 
 
 function App() {
-  const [movies, setMovies] = useState(moviePosters);
-  const [selectedMovie, setSelectedMovie] = useState(false);
-  
-  function movieClick() {
-    setSelectedMovie(movieDetailsData);
+  const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  const [error, setError] = useState('')
+	
+  useEffect(() => {
+    getMovies();
+  }, [])
+
+  function movieClick(data) {
+    setSelectedMovie(data);
   }
   
   function homeClick() {
     setSelectedMovie(false);
   }
+
+  function getMovies() {
+    fetch('https://rancid-tomatillos-api-ce4a3879078e.herokuapp.com/api/v1/movies')
+    .then(response => response.json())
+    .then(data => setMovies([...movies, ...data]))
+    .catch(error => {
+      console.log(error)
+      setError('Oops! Something went wrong! Please try again in a couple minutes.')
+    })
+  }
+
+  function getMovieDetails(movie_id) {
+    fetch(`https://rancid-tomatillos-api-ce4a3879078e.herokuapp.com/api/v1/movies/${movie_id}`)
+    .then(response => response.json())
+		.then(data => movieClick(data))
+    .catch(error => {
+      console.log(error)
+      setError('Oops! Something went wrong! Please try again in a couple minutes.')
+    })
+  }
+
+
   
   const updateVoteCount = (moviePosterId, delta) => {
     //Is there a quicker way to do this?  Would prefer to do a .find, update the key/value of that element, then just set the array
@@ -42,7 +69,12 @@ function App() {
         {selectedMovie && (<button onClick={homeClick}><img src={homeButton} alt="Home" /></button>
         )}
       </header>
-      {!selectedMovie ? ( <MoviesContainer movies={movies} onMovieClick={movieClick} updateVoteCount={updateVoteCount} />) : (
+      {!selectedMovie ? ( 
+				<MoviesContainer 
+					movies={movies} 
+				  getMovieDetails={getMovieDetails}
+					updateVoteCount={updateVoteCount} />
+			) : (
         <MovieDetails
           posterPath={selectedMovie.backdrop_path}
           genreIds={selectedMovie.genres}
