d1b4b474bb1071e7d86b4d0cb42571acc3e6cf65
Mark Kendall <mkendall.tutor@gmail.com>
2025-04-07 19:16:53 -0500

    Fix: CSS updates to scale posters and images correctly, preserving aspect ratios

--- src/MoviePoster/MoviePoster.js
+++ src/MoviePoster/MoviePoster.js
@@ -5,11 +5,13 @@ import upvoteIcon from '../icons/upvote.png'
 function MoviePoster({ id, posterPath, voteCount, title, onClick, updateVoteCount }) {
   return (
     <div className='movie-poster'>
-      <button onClick={onClick}><img src={posterPath} alt={`${title} poster`}></img></button>
+      <button class='details-button' onClick={onClick}><img src={posterPath} alt={`${title} poster`}></img></button>
       <h3>{title}</h3>
       <p>
         <button class='vote-button' onClick = {() => updateVoteCount(id, 1)}><img class='movie-poster-button-icons' src={upvoteIcon} alt="up-arrow"></img></button>
-        {voteCount}
+        <div class='vote-count'>
+          {voteCount}
+        </div>
         <button class='vote-button' onClick = {() => updateVoteCount(id, -1)}><img class='movie-poster-button-icons' src={downvoteIcon} alt="down-arrow"></img></button>
       </p>
     </div>
