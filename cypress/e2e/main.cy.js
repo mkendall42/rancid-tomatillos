0ae540dad9a77a210f6232f72afcb4792bb13ec6
little-Patrick <Little.patrick@outlook.com>
2025-04-08 18:09:42 -0600

    Add: cypress testing for the main page and the movie posters that show up on page load

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -1,12 +1,37 @@
 // Mock data to use for testing:
-// import posters from '../fixtures/movie_posters.json' (we've added mock data to this file for you!)
-// import details from '../fixtures/movie_details.json' (you will need to add your own mock data to this file!)
+import posters from '../fixtures/movie_posters.json'
+// import details from '../fixtures/movie_details.json' 
 
 describe('Main Page', () => {
+  beforeEach(() => {
+    cy.intercept("GET", "https://rancid-tomatillos-api-ce4a3879078e.herokuapp.com/api/v1/movies", {
+      statusCode: 200,
+      fixture: "movie_posters"
+    })
+    cy.visit('http://localhost:3000/')
+
+	})
+
   it('displays title on page load', () => {
     // hint: you'll want to add an intercept here if you are making a network request on page load!
-    cy.visit('http://localhost:3000/')
-    .get('h1')
+    cy.get('h1')
     .contains('rancid tomatillos')
   })
+
+	it('displays all movie posters', () => {
+    cy.get('.movie-container').should('exist')
+    .get('.movie-poster').should('have.length', 4)
+	})
+
+  it('has correct first and last movie posters', () => {
+    cy.get('.movie-container').should('exist')
+    .get('.movie-poster').first().find('h3').should('have.text', 'The Dark Knight')
+    .get('.movie-poster').first().find('img').should('exist')
+    .get('.movie-poster').first().find('.vote-count').should('have.text', '32544')
+    .get('.movie-poster').last().find('h3').should('have.text', 'Pulp Fiction')
+    .get('.movie-poster').last().find('img').should('exist')
+    .get('.movie-poster').last().find('.vote-count').should('have.text', '27642')
+  })
 })
+
+
