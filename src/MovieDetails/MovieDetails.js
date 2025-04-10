a23206d73f1ddf2f03683c5eed648ef636f0ffe7
little-Patrick <Little.patrick@outlook.com>
2025-04-10 11:21:42 -0600

    Add: pass full movie prop down

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -1,15 +1,21 @@
 import './MovieDetails.css';
 
-function MovieDetails({posterPath, genreIds, overview, title}) {
+function MovieDetails({ movie }) {
   return (
     <section className='MovieDetails'>
-			<img src={posterPath} alt={`${title} backdrop`} />
-      <h2>{title}</h2>
-      <p>Genres: {genreIds}</p>
-      <article>
-        <h3>Overview</h3>
-        <p>{overview}</p>
-      </article>
+      <img className='backdrop' src={movie.backdrop_path} alt={`${movie.title} backdrop`} />
+      <div className="details-card">
+        <h2>{movie.title}</h2>
+        <div className="genres">
+          {(movie.genre_ids || []).map((genre) => (
+            <div className="genre-tag">{genre}</div>
+          ))}
+        </div>
+        <article>
+          <h3>Overview</h3>
+          <p>{movie.overview}</p>
+        </article>
+      </div>
     </section>
   );
 }
