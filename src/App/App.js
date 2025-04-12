d0a1c229b0c7ad6254bd63947dd633ef2cd5d5db
little-Patrick <Little.patrick@outlook.com>
2025-04-12 09:02:01 -0600

    Fix: Formatting for the dom

--- src/App/App.js
+++ src/App/App.js
@@ -6,111 +6,98 @@ import MovieDetails from '../MovieDetails/MovieDetails.js'
 import ErrorPage from '../ErrorPage/ErrorPage.js'
 import MoviesContainer from '../MoviesContainer/MoviesContainer';
 import SearchForm from '../SearchForm/SearchForm'
-import { Routes, Route, NavLink} from 'react-router-dom'
-
+import { Routes, Route, NavLink } from 'react-router-dom'
 
 function App() {
-  const [movies, setMovies] = useState([]);
-  const [selectedMovie, setSelectedMovie] = useState(null);
-  const [error, setError] = useState('')
-  const [searchedMovies, setSearchedMovies] = useState(null)
+	const [movies, setMovies] = useState([]);
+	const [selectedMovie, setSelectedMovie] = useState(null);
+	const [error, setError] = useState('')
+	const [searchedMovies, setSearchedMovies] = useState(null)
 
-	
-  useEffect(() => {
-    fetch('https://rancid-tomatillos-api-ce4a3879078e.herokuapp.com/api/v1/movies')
-    .then(response => response.json())
-    .then(data => setMovies([...movies, ...data]))
-    .catch(error => {
-      console.log(error)
-      setError('Oops! Something went wrong! Please try again in a couple minutes.')
-    })
-  }, [])
 
-  // function movieClick(data) {
-  //   setSelectedMovie(data);
-  // }
-  
-  // function homeClick() {
-  //   setSelectedMovie(null);
-  // }
+	useEffect(() => {
+		fetch('https://rancid-tomatillos-api-ce4a3879078e.herokuapp.com/api/v1/movies')
+			.then(response => response.json())
+			.then(data => setMovies([...movies, ...data]))
+			.catch(error => {
+				console.log(error)
+				setError('Oops! Something went wrong! Please try again in a couple minutes.')
+			})
+	}, [])
 
-  // function getMovies() {
+	const getMovieDetails = (movie_id) => {
+		console.log("I'm here!")
 
-  // }
+		const data = fetch(`https://rancid-tomatillos-api-ce4a3879078e.herokuapp.com/api/v1/movies/${movie_id}`)
+			.then(response => response.json())
+			.then(data => { setSelectedMovie(data) })
+			.catch(error => {
+				console.log(error)
+				setError('Oops! Something went wrong! Please try again in a couple minutes.')
+			})
 
-  const getMovieDetails = (movie_id) => {
-    console.log("I'm here!")
+		return data
+	}
 
-    const data = fetch(`https://rancid-tomatillos-api-ce4a3879078e.herokuapp.com/api/v1/movies/${movie_id}`)
-    .then(response => response.json())
-		.then(data => { setSelectedMovie(data) })
-    .catch(error => {
-      console.log(error)
-      setError('Oops! Something went wrong! Please try again in a couple minutes.')
-    })
+	const updateVoteCount = (moviePosterId, delta) => {
+		//Is there a quicker way to do this?  Would prefer to do a .find, update the key/value of that element, then just set the array
+		//NOTE: optional - ensure that vount count cannot be negative
+		let old_vote_count = 0
+		const updatedMovies = movies.reduce((acc, movie) => {
+			if (movie.id === moviePosterId) {
+				old_vote_count = movie.vote_count
+				movie.vote_count += delta
+			}
 
-    return data
-  }
+			acc.push(movie)
+			return acc
+		}, [])
 
-  const updateVoteCount = (moviePosterId, delta) => {
-    //Is there a quicker way to do this?  Would prefer to do a .find, update the key/value of that element, then just set the array
-    //NOTE: optional - ensure that vount count cannot be negative
-    let old_vote_count = 0
-    const updatedMovies = movies.reduce((acc, movie) => {
-      if (movie.id === moviePosterId) {
-        old_vote_count = movie.vote_count
-        movie.vote_count += delta
-      }
+		setMovies(updatedMovies)
 
-      acc.push(movie)
-      return acc
-    }, [])
+		//Update the API and ensure vote count changed correctly
+		//NOTE: in order for vote count to persist between refresh, moviePosters must be fetched from API first (perhaps only at mount?)
+		let direction = ""
+		if (delta > 0) {
+			direction = "up"
+		} else if (delta < 0) {
+			direction = "down"
+		} else {
+			console.log("Error: client: invalid vote change.")
+		}
 
-    setMovies(updatedMovies)
+		const parameters = {
+			method: "PATCH",
+			body: JSON.stringify({ vote_direction: direction }),
+			headers: { "Content-Type": "application/json" }
+		}
 
-    //Update the API and ensure vote count changed correctly
-    //NOTE: in order for vote count to persist between refresh, moviePosters must be fetched from API first (perhaps only at mount?)
-    let direction = ""
-    if (delta > 0) {
-      direction = "up"
-    } else if (delta < 0) {
-      direction = "down"
-    } else {
-      console.log("Error: client: invalid vote change.")
-    }
+		fetch(`https://rancid-tomatillos-api-ce4a3879078e.herokuapp.com/api/v1/movies/${moviePosterId}`, parameters)
+			.then(response => response.json())
+			.then(data => verifyVoteCountChange(old_vote_count, data.vote_count, delta))
+			.catch(error => console.log(error))
+	}
 
-    const parameters = {
-      method: "PATCH",
-      body: JSON.stringify({ vote_direction: direction }),
-      headers: { "Content-Type": "application/json" }
-    }
+	function verifyVoteCountChange(old_count, new_count, delta) {
+		if (new_count !== old_count + delta) {
+			console.log("Error: server/client: vote count did not update correctly relative to old value.")
+		}
+	}
 
-    fetch(`https://rancid-tomatillos-api-ce4a3879078e.herokuapp.com/api/v1/movies/${moviePosterId}`, parameters)
-      .then(response => response.json())
-      .then(data => verifyVoteCountChange(old_vote_count, data.vote_count, delta))
-      .catch(error => console.log(error))
-  }
-
-  function verifyVoteCountChange(old_count, new_count, delta) {
-    if (new_count !== old_count + delta) {
-      console.log("Error: server/client: vote count did not update correctly relative to old value.")
-    }
-  }
-
-  return (
-    <main className='App'>
-      <header>
-        <h1>rancid tomatillos</h1>
-        <NavLink to="/" className={({ isActive }) => !isActive ? "home-button" : "hidden"}><img src={homeButton} alt="Home" /></NavLink>
-        <SearchForm movies={movies} setSearchedMovies={setSearchedMovies} />
-      </header>
-      <Routes>
-        <Route path="/" element={<MoviesContainer movies={movies} searchedMovies={searchedMovies} getMovieDetails={getMovieDetails} updateVoteCount={updateVoteCount} />} />
-        <Route path='/:movie_id' element={<MovieDetails selectedMovie={selectedMovie} getMovieDetails={getMovieDetails} />} />
-        <Route path="*" element={<ErrorPage error={error} />} />
-      </Routes>
-    </main>
-  )
+	return (
+		<main className='App'>
+			<header>
+				<h1>rancid tomatillos</h1>
+				<NavLink to="/" className={({ isActive }) => !isActive ? "home-button" : "hidden"}><img src={homeButton} alt="Home" /></NavLink>
+				<SearchForm movies={movies} setSearchedMovies={setSearchedMovies} />
+			</header>
+			<Routes>
+				<Route path="/" element={<MoviesContainer movies={movies} searchedMovies={searchedMovies} getMovieDetails={getMovieDetails} updateVoteCount={updateVoteCount} />} />
+				<Route path='/:movie_id' element={<MovieDetails selectedMovie={selectedMovie} getMovieDetails={getMovieDetails} />} />
+				<Route path="*" element={<ErrorPage error={error} />} />
+			</Routes>
+		</main>
+	)
 }
 
 export default App;
