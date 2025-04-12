4ca39c1e4543ff4e8d4291c58103d7aa7f2cad03
little-Patrick <Little.patrick@outlook.com>
2025-04-12 09:02:13 -0600

    Feat: Search function functional

--- src/SearchForm/SearchForm.js
+++ src/SearchForm/SearchForm.js
@@ -2,40 +2,34 @@ import { useState } from 'react';
 import './SearchForm.css';
 
 function SearchForm({ movies, setSearchedMovies }) {
-  const [searchText, setSearchText] = useState('');
-//   const [searchedMovies, setSearchedMovies] = useState(movies)
+	const [searchText, setSearchText] = useState('');
 
-//   function clearInput() {
-//     setTitle('');
-//     setDescription('');
-//   }
+	function weLikeFiniteLoops(event) {
+		let input = event.target.value
+		setSearchText(input)
 
-  function weLikeFiniteLoops(event) {
-      setSearchText(event.target.value)
+		if (event.target.value === '') {
+			setSearchedMovies(movies);
+			return;
+		}
 
-      if (event.target.value = '') {
-        setSearchedMovies(null)
-      }
-      
-      const filteredMovies = movies.filter(movie => {
-        console.log(searchText)
-        return movie.title.includes(searchText)
-      })
+		const filteredMovies = movies.filter(movie =>
+			movie.title.toLowerCase().includes(input.toLowerCase())
+		);
+		setSearchedMovies(filteredMovies)
+	}
 
-      setSearchedMovies(filteredMovies)
-    }
-
-  return (
-    <form>
-    <input
-      type='text'
-      placeholder='Enter your search term here'
-      name='title'
-      value={searchText}
-      onChange={event => weLikeFiniteLoops(event)}
-    />
-  </form>
-  )
+	return (
+		<form>
+			<input
+				type='text'
+				placeholder='Enter your search term here'
+				name='title'
+				value={searchText}
+				onChange={event => weLikeFiniteLoops(event)}
+			/>
+		</form>
+	)
 }
 
 export default SearchForm
